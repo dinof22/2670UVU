17f76819dfb75eeb71c22b031b637f6cc77739f1
Unknown <jakeshmearlof4@gmail.com>
2017-08-30 14:23:13 -0600

    character move, animation, and flip

---
+++ Scripting2670/Assets/Scripts/AnmateCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnmateCharacter : MonoBehaviour {
+
+    Animator anims;
+
+	// Use this for initialization
+	void Start () {
+        anims = GetComponent<Animator>();
+        MoveInputs.KeyAction += Animate;
+	}
+
+    private void Animate(float obj)
+    {
+        anims.SetFloat("Walk", obj);
+    }
+}
--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -8,15 +8,19 @@ public class MoveCharacter : MonoBehaviour {
 
     CharacterController cc;
 
+    Vector3 tempMove;
+
+    public float speed = 5;
 
     void Start()
     {
         cc = GetComponent<CharacterController>();
-        MoveInputs.KeyAction = Move;
+        MoveInputs.KeyAction += Move;
     }
 
     void Move(float _movement)
     {
-        print(_movement);
+        tempMove.x = _movement * speed * Time.deltaTime;
+        cc.Move(tempMove);
     }
 }
