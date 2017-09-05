0613660bc832996ae6cb97cba17e136ecb7d4947
Unknown <jakeshmearlof4@gmail.com>
2017-09-04 23:58:33 -0700

    Finished grab mechanic

--- Bear Prototypes/Assets/grabbableScript.cs
+++ Bear Prototypes/Assets/grabbableScript.cs
@@ -5,21 +5,54 @@ using System;
 
 public class grabbableScript : MonoBehaviour {
 
-    public GameObject SolidCube;
-    
 
 
-    private void OnTriggerEnter(Collider other)
+    public GameObject grabHolder;
+
+    private void Update()
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            print("you touched teh box");
-            //SolidCube.transform.position = new Vector3(44, 55, 0);    //this WORKS
-            if (Input.GetKeyDown(KeyCode.LeftControl))
-            {
-                print("grabbing!!!");
-                //transform.root = cc.gameObject.transform;
-            }
+            print("control Pressed");
+            //GetComponent<Rigidbody>().useGravity = false;
+            //this.transform.position = new Vector3(grabHolder.transform.position.x, grabHolder.transform.position.y, 0f);
+            //this.transform.eulerAngles = new Vector3(0, 0, 0);
+            this.transform.parent = grabHolder.transform;
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            print("control let go");
+            this.transform.parent = null;
+            //GetComponent<Rigidbody>().useGravity = true;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+    //public GameObject SolidCube;
+
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        print("you touched teh box");
+    //        //SolidCube.transform.position = new Vector3(44, 55, 0);    //this WORKS
+    //        if (Input.GetKeyDown(KeyCode.LeftControl))
+    //        {
+    //            print("grabbing!!!");
+    //            //transform.root = cc.gameObject.transform;
+    //        }
+    //    }
+    //}
 }
