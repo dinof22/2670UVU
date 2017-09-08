98b34a35ab05687e9d148126cb9de9cf2c94db50
Unknown <jakeshmearlof4@gmail.com>
2017-09-08 14:02:48 -0600

    added scene loading!

---
+++ Scripting2670/Assets/Scripts/Load.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class Load : MonoBehaviour {
+
+
+    public static Action EndButtons;
+
+
+
+    private void Start()
+    {
+        Load.EndButtons += EndThis;
+    }
+
+    private void EndThis()
+    {
+        GetComponent<Button>().interactable = false;
+    }
+
+
+
+
+
+    public string scene;
+
+    public void loadLevel()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        EndButtons();
+    }
+}
