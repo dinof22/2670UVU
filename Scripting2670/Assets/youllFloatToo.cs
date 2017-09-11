b5014aa991e1ab08c68b37ce557c655466b15848
Unknown <jakeshmearlof4@gmail.com>
2017-09-11 16:05:03 -0600

    did... stuff?

---
+++ Scripting2670/Assets/youllFloatToo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class youllFloatToo : MonoBehaviour {
+
+    public Vector3 moveIt;
+
+    IEnumerator floatTheBallooon()
+    {
+        while (moveIt.x < 0.3f)
+        {
+            print("youll float too");
+            moveIt.x += 0.003f * Time.deltaTime;
+            transform.Translate(moveIt);
+            yield return null;
+        }
+    }
+
+	void Start () {
+        StartCoroutine(floatTheBallooon());
+	}
+
+
+
+
+}
