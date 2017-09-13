8e243410e814213409589731c0baf1ed2ff55355
Unknown <jakeshmearlof4@gmail.com>
2017-09-13 13:24:57 -0600

    tred to tweak monkeybar script

--- Bear Prototypes/Assets/Scripts/MonkeybarScript.cs
+++ Bear Prototypes/Assets/Scripts/MonkeybarScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MonkeybarScript : MonoBehaviour {
 
@@ -8,7 +9,7 @@ public class MonkeybarScript : MonoBehaviour {
 
     CharacterMovement cc;
 
-    private float fakeGravity;
+    //private float fakeGravity;
 
     private void Start()
     {
@@ -17,28 +18,63 @@ public class MonkeybarScript : MonoBehaviour {
         
     }
 
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.LeftControl))
+    //        {
+    //            print("controlPressed");
+    //           cc.gravity = 0f;
+    //        }
+    //        if (Input.GetKeyUp(KeyCode.LeftControl))
+    //        {
+    //            print("left control released!!");
+    //            cc.gravity = 40f;
+    //        }
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        cc.gravity = 40f;
+    //    }
+    //}
+
+
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.LeftControl))
-            {
-                print("controlPressed");
-               cc.gravity = 0f;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftControl))
-            {
-                print("left control released!!");
-                cc.gravity = 40f;
-            }
+            MoveInput.Grab += GrabHandler;
+            player = other.gameObject;
+            MoveInput.offGrab += offGrabHandler;
         }
     }
 
+    
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.tag == "Player")
         {
+            MoveInput.Grab -= GrabHandler;
             cc.gravity = 40f;
         }
     }
+
+    private void GrabHandler()
+    {
+        cc.gravity = 0f;
+    }
+
+    private void offGrabHandler()
+        {
+        cc.gravity = 40f;
+        }
+
 }
