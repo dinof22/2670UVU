a9f5c7e1031388649daf935e38a750f37a3204a3
Unknown <jakeshmearlof4@gmail.com>
2017-09-26 20:58:17 -0600

    added scripts for bera movovment

---
+++ Bear Prototypes/Assets/BearMoveInputs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BearMoveInputs : MonoBehaviour {
+
+
+    public static Action<float> BearKeyAction;
+
+    public static Action BearJumpAction;
+    public static Action BearConstantAction;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            BearJumpAction();
+        }
+
+
+        if (BearKeyAction != null)
+        {
+            BearKeyAction(Input.GetAxis("HorizontalBear"));
+        }
+
+
+        if (constantAction != null)
+        {
+            constantAction();
+        }
+
+    }
+}
