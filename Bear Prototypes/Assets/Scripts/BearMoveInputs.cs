a7c411db77f378712318d72afc2f14e43bb8d39e
Unknown <jakeshmearlof4@gmail.com>
2017-09-29 16:35:13 -0600

    Added alligator Ai

---
+++ Bear Prototypes/Assets/Scripts/BearMoveInputs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BearMoveInputs : MonoBehaviour {
+
+
+    public static Action<float> BearKeyAction;
+
+    public static Action BearJumpAction;
+    public static Action BearConstantAction;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            BearJumpAction();
+        }
+
+
+        if (BearKeyAction != null)
+        {
+            BearKeyAction(Input.GetAxis("Horizontal2"));
+        }
+
+
+        if (BearConstantAction != null)
+        {
+            BearConstantAction();
+        }
+
+    }
+}
---
+++ Bear Prototypes/Assets/Scripts/bearRidingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bearRidingScript : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            other.transform.parent = null;
+        }
+    }
+}
