995bd5180cf705edd15937bc16d0ee2feaca0ce1
Unknown <jakeshmearlof4@gmail.com>
2017-10-11 00:43:52 -0600

    made the lights have raycasting

---
+++ Bear Prototypes/Assets/LightRaycastingRay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LightRaycastingRay : MonoBehaviour {
+
+
+    public static Action turnOffLight;
+
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            //Debug.Log(hit.transform.name);
+            turnOffLight();
+
+
+            TurnOnLightScript target = hit.transform.GetComponent<TurnOnLightScript>();
+
+
+            if (target != null)
+            {
+                target.TurnOnLights();
+            }
+
+
+        }
+
+    }
+}
---
+++ Bear Prototypes/Assets/TurnOnLightScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TurnOnLightScript : MonoBehaviour {
+
+    //private Transform ThisThingsChild;
+
+    private void Start()
+    {
+        LightRaycastingRay.turnOffLight += TurnOffLightHandler;
+    }
+
+    public void TurnOnLights()
+    {
+        //print(this.transform.GetChild(0).name);
+        this.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void TurnOffLightHandler()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+}
